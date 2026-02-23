e8f9ca6ae9027b1e6d943e9699db506848eddc0b
Brendon Dal <bmdal783@gmail.com>
2026-02-22 21:33:18 -0600

    Code for question 3

---
+++ Making_cd_index.py
@@ -0,0 +1,54 @@
+#brendon dal comp 483
+from Bio import SeqIO
+
+fasta = "ncbi_dataset/data/GCF_000845245.1/GCF_000845245.1_ViralProj14559_genomic.fna"
+gff_file = "ncbi_dataset/data/GCF_000845245.1/genomic.gff"
+output_fasta = "HCMV_CDS.fna"
+
+# Load genome (HCMV has one chromosome)
+record = SeqIO.read(fasta, "fasta")
+genome_seq = record.seq
+
+cds_count = 0
+
+with open(gff_file) as gff, open(output_fasta, "w") as out:
+    for line in gff:
+        if line.startswith("#"):
+            continue
+
+        fields = line.strip().split("\t")
+        if len(fields) != 9:
+            continue
+
+        seqid, source, feature_type, start, end, score, strand, phase, attributes = fields
+
+        if feature_type != "CDS":
+            continue
+
+        # Parse attributes field
+        attr_dict = {}
+        for attr in attributes.split(";"):
+            if "=" in attr:
+                key, value = attr.split("=", 1)
+                attr_dict[key] = value
+
+        if "protein_id" not in attr_dict:
+            continue
+
+        protein_id = attr_dict["protein_id"]
+
+        # GFF is 1-based inclusive
+        start = int(start) - 1
+        end = int(end)
+
+        cds_seq = genome_seq[start:end]
+
+        if strand == "-":
+            cds_seq = cds_seq.reverse_complement()
+
+        out.write(f">{protein_id}\n")
+        out.write(str(cds_seq) + "\n")
+
+        cds_count += 1
+
+print(f"The HCMV genome (GCF_000845245.1) has {cds_count} CDS.")
