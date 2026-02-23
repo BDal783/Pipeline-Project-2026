2c2370d19e972fcf52e50d6032a9136d3397e559
Brendon Dal <bmdal783@gmail.com>
2026-02-23 15:24:53 -0600

    working code

--- Blast.py
+++ Blast.py
@@ -1,19 +1,25 @@
 from pathlib import Path
 import os
 from Bio import SeqIO
-
+###### go back and undo commented out os
 contig_files = sorted(
     Path(".").glob("*_assembly/contigs.fasta")
 )
+#print(contig_files)
 
 #downloading data base for betaherpsvirinae
 subfamily = 'datasets download virus genome taxon Betaherpesvirinae --include genome'
-os.system(subfamily)
-os.system("unzip ncbi_dataset.zip")
+#os.system(subfamily)
+#os.system("unzip ncbi_dataset.zip")
+
+
+with open("Dal_PipelineReport.txt", 'a') as prompt:
+    prompt.write("\nProblem6\n")
+    prompt.close()
 
 #making database
 db = 'makeblastdb -in ncbi_dataset/data/genomic.fna -out Betaherpesbirinae -title Betaherpesbirinae -dbtype nucl'
-os.system(db)
+#os.system(db)
 
 
 with open("longestContig.txt", "w") as report:
@@ -28,17 +34,25 @@ with open("longestContig.txt", "w") as report:
 
         longest_fasta = f"{sample}_longest.fasta"
         SeqIO.write(longest, longest_fasta, "fasta")
+        name = str(contig_path)[:10]
+        #print(name)
+        
+        blast_out = name+".blast.txt"
 
-        report.write(f"{sample}\n")
-
-        blast_command = 'blastn -query '+longest_fasta+' -db Betaherpesbirinae -outfmt \"6 sacc pident length qstart qend sstart send bitscore evalue stitle\" -out {sample}.blast.txt'
+        blast_command = 'blastn -query '+longest_fasta+' -db Betaherpesbirinae -outfmt \"6 sacc pident length qstart qend sstart send bitscore evalue stitle\" -out '+blast_out
         
         os.system(blast_command)
 
-        with open('{sample}.blast.txt', 'r') as f:
-            lines = f.readlines()
+        with open(name+".blast.txt", 'r') as f:
+            lines = f.readlines()[:5]
+            #print(lines)
             f.close()
         with open("Dal_PipelineReport.txt", 'a') as file:
+            file.write(name+"\n")
+            file.write('sacc\tpident\tlength\tqstart\tqend\tsstart\tsend\tbitscore\tevalue\tstitle\n')
             for i in lines:
                 file.write(i)
+                #print(i)
+            file.write("\n")
             file.close()
+    
