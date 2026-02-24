cea953bbf1a2e943373153b722dd98415c0160f3
BDal783 <150982020+BDal783@users.noreply.github.com>
2026-02-23 22:44:50 -0600

    added comments

--- Blast.py
+++ Blast.py
@@ -1,18 +1,19 @@
+#libraries
 from pathlib import Path
 import os
 from Bio import SeqIO
-###### go back and undo commented out os
+#grabbing file
 contig_files = sorted(
     Path(".").glob("*_assembly/contigs.fasta")
 )
 #print(contig_files)
 
-#downloading data base for betaherpsvirinae
+#downloading data base for betaherpsvirinae and unzipping it
 subfamily = 'datasets download virus genome taxon Betaherpesvirinae --include genome'
 os.system(subfamily)
 os.system("unzip ncbi_dataset.zip")
 
-
+#writing the problem number to report for clarity
 with open("PipelineReport.txt", 'a') as prompt:
     prompt.write("\nProblem6\n")
     prompt.close()
@@ -21,7 +22,7 @@ with open("PipelineReport.txt", 'a') as prompt:
 db = 'makeblastdb -in ncbi_dataset/data/genomic.fna -out Betaherpesbirinae -title Betaherpesbirinae -dbtype nucl'
 os.system(db)
 
-
+#opening longest contig file to blast each one
 with open("longestContig.txt", "w") as report:
 
     for contig_path in contig_files:
@@ -36,13 +37,13 @@ with open("longestContig.txt", "w") as report:
         SeqIO.write(longest, longest_fasta, "fasta")
         name = str(contig_path)[:10]
         #print(name)
-        
+        #establishing blast out file name
         blast_out = name+".blast.txt"
-
+        #blast command
         blast_command = 'blastn -query '+longest_fasta+' -db Betaherpesbirinae -outfmt \"6 sacc pident length qstart qend sstart send bitscore evalue stitle\" -out '+blast_out
         
         os.system(blast_command)
-
+        #grabbing top 5 and printing to report
         with open(name+".blast.txt", 'r') as f:
             lines = f.readlines()[:5]
             #print(lines)
